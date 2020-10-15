a320527a295393f1a9acef9e90cd6d03bcecfc08
PattyRodriguezGt <72937032+PattyRodriguezGt@users.noreply.github.com>
2020-10-15 11:46:07 -0600

    Update main.js
    -agregando ejercicio de promesas

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ var concatenacion=nombre + ' '+ altura;
 var datos=document.getElementById('datos').innerHTML=`
 <h1> hola soy la caja de datos</h1>
 <h2>nombre ${nombre}</h2>`;
+
+	// promesass
+var saludar=new Promise((resolve,reject) =>
+{
+	setTimeout(()=>
+	{
+		let saludo= "Hola a todos!";
+		if(saludo)
+			{resolve(saludo);}
+		else
+		{
+			reject('No hay saludo disponible');
+		}
+	},2000);
+	
+});
+
+saludar.then(resultado => alert(resultado)).catch(err => alert(err));
