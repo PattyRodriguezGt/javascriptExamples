c8e9ef0abf15a0567241184946d5b4b4b99b4755
PattyRodriguezGt <72937032+PattyRodriguezGt@users.noreply.github.com>
2020-10-15 10:45:55 -0600

    Primer Commit del contenido del repaso

---
+++ main.js
@@ -0,0 +1,6 @@
+var nombre="Patricia Rodriguez";
+var altura=161;
+var concatenacion=nombre + ' '+ altura;
+var datos=document.getElementById('datos').innerHTML=`
+<h1> hola soy la caja de datos</h1>
+<h2>nombre ${nombre}</h2>`;
